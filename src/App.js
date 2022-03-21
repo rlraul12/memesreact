f75964d92df846c8cd9a2bcaef6fc3b433889c28
Raul E. Paneque <49737084+rlraul12@users.noreply.github.com>
2022-03-21 15:51:15 -0400

    ultimando estilos de exportacion

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ function App() {
 
   const onChangeExport = function(){
     alert('Exportando');
-    html2canvas(document.querySelector("#meme")).then(canvas=>{
+    html2canvas(document.querySelector(".meme")).then(canvas=>{
       var img = canvas.toDataURL("image/png");
 
       var link = document.createElement('a');
       link.download='meme.png';
       link.href=img;
       link.click();
-      
+
     });
   }
 
@@ -46,6 +46,7 @@ function App() {
         <option value="matrix">Matrix</option>
         <option value="philosoraptor">Philosoraptor</option>
         <option value="smart">Smart Guy</option>
+        <option value="unicornios">unicornio</option>
       </select>
       <br/>
       <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'/><br/>
